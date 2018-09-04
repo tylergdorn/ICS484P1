354e3dfec968e67b28ab450cb2716ec34034868b
Tyler Dorn <tylergdorn@gmail.com>
2018-09-03 14:55:30 -1000

    Working on loading data with fetch
    need to make fetch work, but fetch should be a better end result than xhr
    also: live server extension gud

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert(1);
+
+// $(" #test ").text = requestLocalData("Cholera/choleraDeaths.tsv");
+// console.log(requestLocalData("Cholera/choleraDeaths.tsv"));
+
+var deathstsv// = Plotly.d3.tsv.parse(requestLocalData("Cholera/choleraDeaths.tsv"));
+// console.log(deathstsv);
+
+fetch('cholera/choleraDeaths.tsv')
+.then(data.text() => console.log(data.text()));
+// TODO: Make this work lmao
