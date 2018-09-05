5d598b1fd234fc53e4d6fd632d0e38ae97c6dfd9
tylergdorn <31430148+tylergdorn@users.noreply.github.com>
2018-09-04 14:21:09 -1000

    because i forgot to commit

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 // $(" #test ").text = requestLocalData("Cholera/choleraDeaths.tsv");
 // console.log(requestLocalData("Cholera/choleraDeaths.tsv"));
 
-var deathstsv// = Plotly.d3.tsv.parse(requestLocalData("Cholera/choleraDeaths.tsv"));
+// = Plotly.d3.tsv.parse(requestLocalData("Cholera/choleraDeaths.tsv"));
 // console.log(deathstsv);
 
 fetch('cholera/choleraDeaths.tsv')
-.then(data.text() => console.log(data.text()));
+// this works but i don't like it
+// .then(function(response){
+//     console.log(response.text())
+// })
+.then((resp) => resp.text())
+.then((resp) => console.log())
+.then(deathp(resp))
+.catch(console.log("something bad happened!"));
 // TODO: Make this work lmao
+
+function deathp(data){
+    var data = Plotly.d3.tsv.parse(data);
+    console.log(data)
+}
+
