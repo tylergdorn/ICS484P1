fdb48ab47d2921554cff686af8e261c7ced1869b
tylergdorn <31430148+tylergdorn@users.noreply.github.com>
2018-09-04 21:39:34 -1000

    Cholera death chart working

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 // $(" #test ").text = requestLocalData("Cholera/choleraDeaths.tsv");
 // console.log(requestLocalData("Cholera/choleraDeaths.tsv"));
-
 // = Plotly.d3.tsv.parse(requestLocalData("Cholera/choleraDeaths.tsv"));
 // console.log(deathstsv);
 
@@ -28,8 +27,43 @@ fetch('cholera/choleraDeaths.tsv')
 // this works now.
 
 function deathp(data){
-    console.log(data);
+    // parse the raw tsv into something useful
     var dataparsed = Plotly.d3.tsv.parse(data);
-    console.log(dataparsed);
+    // initialize our holders for data
+    xData = [];
+    attackData = [];
+    deathData = [];
+    totalData = [];
+    // add all the data to the arrays
+    for(i in dataparsed){
+        xData[i] = dataparsed[i].Date;
+        attackData[i] = dataparsed[i].Attack;
+        deathData[i] = dataparsed[i].Death;
+        totalData[i] = parseInt(deathData[i]) + parseInt(attackData[i]);
+    }
+    var attackTrace = {
+        x: xData,
+        y: attackData,
+        type: 'bar',
+        name: 'Cholera Attacks'
+    };
+    var deathTrace = {
+        x: xData,
+        y: deathData,
+        type: 'bar',
+        name: 'Cholera Deaths'
+    };
+    var totalTrace = {
+        x: xData,
+        y: totalData,
+        type: 'bar',
+        name: 'Total Cholera Incidents'
+    };
+    data = [attackTrace, deathTrace, totalTrace];
+    plot2Div = document.getElementById('test');
+    var layout = {
+        title: "Cholera Deaths"
+    };
+   var myChart2=Plotly.plot(plot2Div, data, layout);
 }
 
